5c998e2d6a67d6f6c8c39c33012dae1950d59598
David Sánchez_Peso <dsancpeso@cdm.local>
2024-10-04 14:29:31 +0200

    comit

--- psp/pspProyecto1/src/psp/Ej1.java
+++ psp/pspProyecto1/src/psp/Ej1.java
@@ -1,36 +1,48 @@
 package psp;
 
-public class Ej1 extends Thread{
-    private static int numero = 0;
-    private static int aumento;
+import java.util.ArrayList;
 
-    public Ej1(int numIncrementos) {
-        aumento = numIncrementos;
+public class Ej1 extends Thread{
+    private static Integer numero = 0;
+    private Integer aumento;
+
+    public Ej1(Integer NUM_INCREMENTOS) {
+    aumento = NUM_INCREMENTOS;
     }
 
-    public static int getNumero() {
+    public  int getNumero() {
         return numero;
     }
     @Override
     public void run(){
-        for (int i = 0; i < aumento; i++) {
-            incrementa();
-        }
-    }
+        synchronized (Ej1.numero) {
 
-    private void incrementa() {
-        numero ++;
+            for (int i = 0; i < aumento; i++) {
+                Integer tmp = numero;
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                numero = tmp + 1;
+            }
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
         Ej1 ej1 = null;
-        int NUM_HILOS = 10;
-        int NUM_INCREMENTOS = 10;
-        for (int i = 0; i < NUM_HILOS; i++) {
+        ArrayList<Ej1> hilos = new ArrayList<>();
+        Integer NUM_HILOS = 1000;
+        Integer NUM_INCREMENTOS = 10;
+        for (int i = 0; i < NUM_HILOS ; i++) {
             ej1 = new Ej1(NUM_INCREMENTOS);
             ej1.start();
+            hilos.add(ej1);
         }
-        ej1.join();
+        for(Ej1 ej1Aux : hilos ){
+                ej1Aux.join();
+        }
+
         System.out.println(ej1.getNumero());
     }
 }
