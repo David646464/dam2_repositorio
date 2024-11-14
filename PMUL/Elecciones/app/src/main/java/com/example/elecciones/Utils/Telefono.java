93e93c22aaa0ca3d7f4ffe22b4f677b4b0d22b34
David Sánchez_Peso <dsancpeso@cdm.local>
2024-11-14 14:30:02 +0100

    commit

---
+++ PMUL/Elecciones/app/src/main/java/com/example/elecciones/Utils/Telefono.java
@@ -0,0 +1,26 @@
+package com.example.elecciones.Utils;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.elecciones.R;
+
+public class Telefono extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_telefono);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
