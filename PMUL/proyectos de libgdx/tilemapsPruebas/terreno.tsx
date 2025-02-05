c5d791e50cb7443c58e692f84ab834ed45254131
David646464 <david.sanchez.peso.64@gmail.com>
2025-02-05 20:28:26 +0100

    commit tiledmap

---
+++ PMUL/proyectos de libgdx/tilemapsPruebas/terreno.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="terreno" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../assets/Pixel Art Top Down - Basic v1.1.2/Texture/TX Tileset Grass.png" width="256" height="256"/>
+ <tile id="0" type="cesped">
+  <properties>
+   <property name="Verde" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
