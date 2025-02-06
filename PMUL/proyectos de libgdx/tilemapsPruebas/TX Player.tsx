fa52edce5677ca82607d72b9ddacef5d55e8e701
David646464 <115078430+David646464@users.noreply.github.com>
2025-02-06 14:29:50 +0100

    commit

--- PMUL/proyectos de libgdx/tilemapsPruebas/TX Player.tsx
+++ PMUL/proyectos de libgdx/tilemapsPruebas/TX Player.tsx
@@ -1,4 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="TX Player" tilewidth="32" tileheight="32" tilecount="16" columns="4">
  <image source="../assets/Pixel Art Top Down - Basic v1.1.2/Texture/TX Player.png" width="128" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="player" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="player" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="player" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="player" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="player" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="player" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
